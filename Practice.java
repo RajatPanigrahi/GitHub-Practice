abdd1d9ab78bcef65fd5b5b4eca563cc8677772d
Rajat <rajatpanigrahi96@gmail.com>
2026-02-07 13:29:12 +0530

    My first commit

---
+++ Practice.java
@@ -0,0 +1,8 @@
+public class Practice{
+    public static void main(String arg[]){
+        int age = 30;
+        String name = "Rajat";
+        System.out.println("Age is : " + age);
+        System.out.println("Name is : " + name);
+    }
+}
